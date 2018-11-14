d767771e768ae07f55867d27fb9273baa2bb9919
Ryan Bowman <ryan@circle.co.ke>
2018-11-14 12:36:35 +0000

    date thing

--- js/main.js
+++ js/main.js
@@ -6,27 +6,27 @@ function getTimeRemaining(endtime) {
   var days = Math.floor(t / (1000 * 60 * 60 * 24));
   return {
     'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
+    'days': days
+    // 'hours': hours,
+    // 'minutes': minutes,
+    // 'seconds': seconds
   };
 }
 
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
   var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+  // var hoursSpan = clock.querySelector('.hours');
+  // var minutesSpan = clock.querySelector('.minutes');
+  // var secondsSpan = clock.querySelector('.seconds');
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
 
     daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    // hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    // minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    // secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
     if (t.total <= 0) {
       clearInterval(timeinterval);
